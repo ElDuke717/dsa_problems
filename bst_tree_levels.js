5974d303537a8fa2c6dfb54e7c878d5c02fff1bd
elduke717 <spacematcha@proton.me>
2023-07-20 12:31:57 -0400

    add all solution types for tree levels

--- bst_tree_levels.js
+++ bst_tree_levels.js
@@ -34,7 +34,7 @@ c.right = f;
 // push each level subarray to the levels array
 // return levels array
 
-const treeLevels = (root) => {
+const treeLevelsStack = (root) => {
   // edge case
   if (root === null) return [];
     // declare levels - holds subarrays
@@ -50,23 +50,75 @@ const treeLevels = (root) => {
     } else {
         levels[levelNum].push(node.val)
     }
-    
-    // add nodes in order breadth first
-    if (node.left != null) {
-        stack.push({node: node.left, levelNum: levelNum + 1 });
-    }
+    // add nodes in order depth first
     if (node.right != null) {
         stack.push({node: node.right, levelNum: levelNum + 1 });
     }
-
+    if (node.left != null) {
+        stack.push({node: node.left, levelNum: levelNum + 1 });
+    }
   }
-
   return levels
 }
 
-console.log(treeLevels(a)); // ->
+console.log(treeLevelsStack(a)); // ->
 // [
 //   ['a'],
 //   ['b', 'c'],
 //   ['d', 'e', 'f']
 // ]
+
+const treeLevelsStackQueue = (root) => {
+    // edge case
+    if (root === null) return [];
+      // declare levels - holds subarrays
+    const levels = []; 
+    // declare stack, holds nodes
+    const stack = [{node: root, levelNum:0}];
+    // while loop to iterate through nodes
+    while (stack.length > 0) {
+      // use a stack approach - pop off each                                                   
+      let {node, levelNum } = stack.shift();
+      if (levels.length === levelNum) {
+          levels.push([ node.val ])
+      } else {
+          levels[levelNum].push(node.val)
+      }
+      // add nodes in order breadth first
+      if (node.left != null) {
+        stack.push({node: node.left, levelNum: levelNum + 1 });
+    }
+      if (node.right != null) {
+          stack.push({node: node.right, levelNum: levelNum + 1 });
+      }
+      
+    }
+    return levels
+  }
+  
+  console.log(treeLevelsStackQueue(a)); // ->
+
+  
+  const treeLevelsStackRecursive = (root) => {
+     const levels = [];
+     fillLevels(root, levels, 0);
+     return levels;
+     
+  }
+
+  const fillLevels = (root, levels, levelNum) =>  {
+    if (root === null) return;
+
+    if (levels.length === levelNum) {
+        levels.push([ root.val ])
+    } else {
+        levels[levelNum].push(root.val);
+    }
+
+    fillLevels(root.left, levels, levelNum + 1);
+    fillLevels(root.right, levels, levelNum + 1);
+
+  }
+  
+  console.log(treeLevelsStackRecursive(a)); 
+
