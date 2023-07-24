50443d2617555ae56755293dc5cce6662e398e6e
elduke717 <spacematcha@proton.me>
2023-07-24 10:36:07 -0400

    add solution to compress

--- strings_compress.js
+++ strings_compress.js
@@ -12,7 +12,7 @@ You can assume that the input only contains alphabetic characters.
 // make a string based on the input of the hashmap
 
 
-const compress = (s) => {
+const compress2 = (s) => {
     // object to hold counts
     const counts = {};
     // initiate string to an empty string
@@ -34,35 +34,37 @@ const compress = (s) => {
     return string;
   };
 
-console.log(compress('ccaaatsss')); // -> '2c3at3s'
+console.log(compress2('ccaaatsss')); // -> '2c3at3s'
 // console.log(compress('ssssbbz')); // -> '4s2bz'
 // console.log(compress('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy')); 
 // // -> '127y'
 
 
 // need to count the letters in order, not just get the absolute number.
-const compress2 = (s) => {
-    // object to hold counts
-    const counts = {};
-    // initiate string to an empty string
-    let string = '';
-    // iterate over string, count letters
+const compress = (s) => {
+    // declare count, set to 1
+    let count = 1;
+    // declare string to hold empty string
+    let string = ''
+    // iterate over the string
     for (let i = 0; i < s.length; i++) {
-        if (counts.hasOwnProperty(s[i])) {
-            counts[s[i]] += 1;
-        } else {
-            counts[s[i]] = 1;
-        }
+    // increment count if letter is the same as previous letter
+      if (s[i+1] === s[i]) {
+        count += 1;
+      } else {
+        // add count to string
+        if (count >1 )string += count;
+        // add s[i] to string
+        string += s[i];
+        // reset count to zero
+        count = 1;
+      }
+    // if letter is different, add count to string, reset count to zero
     }
-    
-    // use for in loop to create string from object
-    for (let letter in counts) {
-        if (counts[letter] > 1) string += counts[letter];
-        string += letter;
-    }
+    // return string
     return string;
   };
 
-console.log(compress2('ccaaatsss')); // -> '2c3at3s'
+console.log(compress('ccaaatsss')); // -> '2c3at3s'
 // console.log(compress2('ssssbbz')); // -> '4s2bz'
 // console.log(compress2('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy')); 
