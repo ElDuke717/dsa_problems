cadf8ed27992377bea210fd6597ab178672de139
elduke717 <spacematcha@proton.me>
2023-07-25 15:22:29 -0400

    add solution to five sort

--- arrays_five_sort.js
+++ arrays_five_sort.js
@@ -7,29 +7,34 @@ Elements that are not 5 can appear in any order in the output, as long as all 5s
 */
 
 const fiveSort = (nums) => {
-    // iterate over the array
+    // count variable 
+    let count = 0;
+    // iterate through the array
     for (let i = 0; i < nums.length; i++) {
-    // if element is equal to five, 
-      if (nums[i] === 5) {
-    // splice it out
-       let five = nums.splice(i, 1)
-       console.log(nums)
-       //console.log(five)
-    // add 5 to the end
-        nums.push(five[0]);
+      // if element at index is not equal to 5
+      if (nums[i] !== 5) {
+        // assign nums at count to nums at the curren index
+        nums[count] = nums[i]
+        // increment count
+        count += 1;
       }
-    } 
+    }
+    // while loop is run to fill in rest of array with 5s
+    while (count < nums.length) {
+        nums[count] = 5;
+        count += 1;
+    }
     return nums;
   };
+  
 
-// console.log( fiveSort([12, 5, 1, 5, 12, 7]));
-// // -> [12, 7, 1, 12, 5, 5] 
+console.log( fiveSort([12, 5, 1, 5, 12, 7]));
+// -> [12, 7, 1, 12, 5, 5] 
 
-
-// console.log(fiveSort([5, 2, 5, 6, 5, 1, 10, 2, 5, 5]));
+console.log(fiveSort([5, 2, 5, 6, 5, 1, 10, 2, 5, 5]));
 // -> [2, 2, 10, 6, 1, 5, 5, 5, 5, 5] 
 
 console.log(fiveSort([5, 5, 5, 1, 1, 1, 4]))
 // -> [4, 1, 1, 1, 5, 5, 5] 
 
-// console.log(fiveSort([5, 2, 5, 1, 1, 1, 4]))
+console.log(fiveSort([5, 2, 5, 1, 1, 1, 4]))
