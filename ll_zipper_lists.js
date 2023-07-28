0b2b5080f0cac11986b9c9af7da43b72fea460e5
elduke717 <spacematcha@proton.me>
2023-07-28 14:40:14 -0400

    add solutions to zipperlists

--- ll_zipper_lists.js
+++ ll_zipper_lists.js
@@ -35,18 +35,46 @@ y.next = z;
 // include logic in while loop to add node and skip if one is missing
 
 const zipperLists = (head1, head2) => {
-    // assign head1 the value of current
-    let current1 = head1;
-    let current2 = head2;
-    // assign holder to hold the value of current.next
-    let tail = null;
-    while (current1 || current2) {
-      
-     
+    let current1 = head1.next; // Start from the second node of the first list.
+    let current2 = head2; // Start from the head of the second list.
+    let prev1 = head1; // Keep track of the previous node in the first list.
+  
+    // Iterate through both lists until one of them reaches the end.
+    while (current1 !== null && current2 !== null) {
+      // Save the next nodes in each list.
+      const next1 = current1.next;
+      const next2 = current2.next;
+  
+      // Insert the node from the second list after the current node in the first list.
+      prev1.next = current2;
+      current2.next = current1;
+  
+      // Move the pointers to the next nodes in both lists.
+      prev1 = current1;
+      current1 = next1;
+      current2 = next2;
     }
-    // return head of the zippered list
-    
+  
+    // If there are remaining nodes in the second list, attach them after the last node of the first list.
+    if (current2 !== null) {
+      prev1.next = current2;
+    }
+  
+    // Return the head of the zippered list (head1).
+    return head1;
   };
 
-  zipperLists(a, x);
+console.log(zipperLists(a, x));
 // a -> x -> b -> y -> c -> z
+
+const zipperListsRecursive =  (head1, head2) => {
+    // if both heads are null, return null;
+    if (head1 === null && head2 === null) return null;
+    if (head1 === null) return head2;
+    if (head2 === null) return head1;
+    const next1 = head1.next;
+    const next2 = head2.next;
+    head1.next = head2;
+    head2.next = zipperLists(next1, next2);
+    return head1;
+  };
