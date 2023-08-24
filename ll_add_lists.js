13ec55840b71735603df18f8efa7ea0778e62ec0
elduke717 <spacematcha@proton.me>
2023-08-24 14:19:52 -0400

    addlist solution replacement

--- ll_add_lists.js
+++ ll_add_lists.js
@@ -25,73 +25,115 @@ class Node {
   }
 }
 
-const addLists = (head1, head2) => {
-    // make a helper function to get the values from a linked list and make a number from them
-    const helper = (head) => {
-      let current = head;
-      let value = '';
-      while (current) {
-        value = value + current.val;
-        current = current.next;
-      }
-      return +value;
-    };
-    
-    // result is the sum of helper on both heads
-    const result = String(helper(head1) + helper(head2)).split('').map(Number);
-    
-    // use function to make linked list from the array
-    const createLinkedList = (values) => {
-      if (values.length === 0) return null;
-      let head = new Node(values[0]);
-      let current = head;
-      for (let i = 1; i < values.length; i += 1) {
-        current.next = new Node(values[i]);
-        current = current.next;
-      }
-      return head;
-    };
-    
-    return createLinkedList(result);
-  };
-  
+
+
+
 
 //   621
 // + 354
 // -----
 //   975
 
-// const a1 = new Node(1);
-// const a2 = new Node(2);
-// const a3 = new Node(6);
-// a1.next = a2;
-// a2.next = a3;
-// // 1 -> 2 -> 6
-
-// const b1 = new Node(4);
-// const b2 = new Node(5);
-// const b3 = new Node(3);
-// b1.next = b2;
-// b2.next = b3;
-// // 4 -> 5 -> 3
-
-// console.log(addLists(a1, b1));
-// // 5 -> 7 -> 9
-
-
 const a1 = new Node(1);
-const a2 = new Node(4);
-const a3 = new Node(5);
-const a4 = new Node(7);
+const a2 = new Node(2);
+const a3 = new Node(6);
 a1.next = a2;
 a2.next = a3;
-a3.next = a4;
-// 1 -> 4 -> 5 -> 7
+// 1 -> 2 -> 6
 
-const b1 = new Node(2);
-const b2 = new Node(3);
+const b1 = new Node(4);
+const b2 = new Node(5);
+const b3 = new Node(3);
 b1.next = b2;
-// 2 -> 3 
+b2.next = b3;
+// 4 -> 5 -> 3
 
 console.log(addLists(a1, b1));
+// 5 -> 7 -> 9
+
+// const a1 = new Node(1);
+// const a2 = new Node(4);
+// const a3 = new Node(5);
+// const a4 = new Node(7);
+// a1.next = a2;
+// a2.next = a3;
+// a3.next = a4;
+// // 1 -> 4 -> 5 -> 7
+
+// const b1 = new Node(2);
+// const b2 = new Node(3);
+// b1.next = b2;
+// // 2 -> 3
+
+// console.log(addLists(a1, b1));
 // 3 -> 7 -> 5 -> 7
+
+// const a1 = new Node(9);
+// const a2 = new Node(3);
+// a1.next = a2;
+// // 9 -> 3
+
+// const b1 = new Node(7);
+// const b2 = new Node(4);
+// b1.next = b2;
+// // 7 -> 4
+
+// console.log(addLists(a1, b1));
+// 6 -> 8
+
+
+
+/*
+
+const addLists = (head1, head2) => {
+  // helper function to reverse a linked list
+  const reverser = (head) => {
+    let prev = null;
+    let current = head;
+
+    while (current) {
+      let nextNode = current.next;
+      current.next = prev;
+      prev = current;
+      current = nextNode;
+    }
+    return prev; // 'prev' now points to the new head of the reversed list
+  };
+  const backward1 = reverser(head1);
+  console.log(backward1)
+  const backward2 = reverser(head2);
+  console.log(backward2)
+  // make a helper function to get the values from a linked list and make a number from them
+  const numberizer = (head) => {
+    let current = head;
+    let value = '';
+    while (current) {
+      value = value + current.val;
+      current = current.next;
+    }
+    return +value;
+  };
+
+  // result is the sum of helper on both heads
+  const result = String(numberizer(backward1) + numberizer(backward2))
+    .split('')
+    .map(Number);
+    console.log(result)
+
+  // use function to make linked list from the array
+  const createLinkedList = (values) => {
+    if (values.length === 0) return null;
+    let head = new Node(values[0]);
+    let current = head;
+    // for loop makes each value a new node in the linked list
+    for (let i = 1; i < values.length; i += 1) {
+      current.next = new Node(values[i]);
+      current = current.next;
+    }
+    return head;
+  };
+  // we need to then reverse the linked list
+  return createLinkedList(result);
+};
+
+*/
